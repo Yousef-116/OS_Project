2feebe5e7c9e8ff3e760b5ca9313534817be2977
UC <yousefgus111@gmail.com>
2023-11-19 13:31:28 +0200

    placement passed

--- kern/trap/fault_handler.c
+++ kern/trap/fault_handler.c
@@ -135,6 +135,11 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 			LIST_INSERT_TAIL(&(curenv->page_WS_list), WSElem);
 		}
 
+		// update page_last_WS_element for FIFO and clock algorithm
+		if(LIST_SIZE(&(curenv->page_WS_list)) == curenv->page_WS_max_size)
+		{
+			curenv->page_last_WS_element = LIST_FIRST(&curenv->page_WS_list);
+		}
 
 		//refer to the project presentation and documentation for details
 	}
