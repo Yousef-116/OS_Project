6278016903042211714dbdbd07e646a45d7ee548
Yousef Mostafa <yousef.com1162003@gmail.com>
2023-12-05 21:31:59 +0200

    add Initialize Scheduler

--- kern/cpu/sched.c
+++ kern/cpu/sched.c
@@ -165,8 +165,18 @@ void sched_init_BSD(uint8 numOfLevels, uint8 quantum)
 	//TODO: [PROJECT'23.MS3 - #4] [2] BSD SCHEDULER - sched_init_BSD
 	//Your code is here
 	//Comment the following line
-	panic("Not implemented yet");
+	//panic("Not implemented yet");
+	*quantums = quantum;
 
+	// struct Env_Queue*
+	for(int level = 0 ; level < numOfLevels ; level++)
+	{
+		//struct Env_Queue *new_Queue;
+		//struct Env_Queue env_ready_queues[level] = &new_Queue;
+		struct Env_Queue env_ready_queues[level];
+	}
+
+	num_of_ready_queues = numOfLevels;
 	//=========================================
 	//DON'T CHANGE THESE LINES=================
 	scheduler_status = SCH_STOPPED;
